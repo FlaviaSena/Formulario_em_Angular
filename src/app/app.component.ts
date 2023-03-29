5fe71f8d7fbb6b2c36fd6b914542fc2115a5a4e1
flavia sena <flaviatecnologia2023@gmail.com>
2023-03-29 17:07:00 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder,ReactiveFormsModule  } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'formulario';
+  cadastro = this.formBuilder.group({ 
+        nome: '', 
+        email: ''});
+
+  constructor(private formBuilder: FormBuilder ){
+    
+  }
+
+
+
+  mensagem(){
+    window.alert("A mensagem");
+  }
+
+  onSubmit(){
+    let obj = this.cadastro.value;
+    console.log(this.cadastro.value);
+    //this.cadastro
+    alert(obj.nome);
+    
+  }
+}
